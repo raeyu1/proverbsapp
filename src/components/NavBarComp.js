ce05532ee9db0653c324e4298f39aad0cf7c457f
raeyu1 <89134763+raeyu1@users.noreply.github.com>
2023-12-16 12:21:03 +0800

    Update NavBarComp.js

--- src/components/NavBarComp.js
+++ src/components/NavBarComp.js
@@ -1,42 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import { BsBook } from 'react-icons/bs'; // Import the Bible icon
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import { BsBook } from 'react-icons/bs';
 import About from './About';
 import Home from './Home';
 
-export default class NavBarComp extends Component {
-  render() {
-    return ( 
-      <Router>
-        <Navbar bg="dark" variant="dark" expand="lg">
-          <Container>
-            <BsBook style={{ fontSize: '30px', marginRight: '10px', color: '#FFFFFF' }} />
-            <Navbar.Brand href="home">Proverbs App</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link as={Link} to="home">
-                  Home
-                </Nav.Link>
-                <Nav.Link as={Link} to="about">
-                  About
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-        <div>
-          <Switch>
-            <Route path="/home">
-              <Home />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
-}
+const NavBarComp = () => {
+  return (
+    <Router>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <BsBook style={{ fontSize: '30px', marginRight: '10px', color: '#FFFFFF' }} />
+          <Navbar.Brand href={`${process.env.PUBLIC_URL}/home`}>
+            Proverbs App
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to={`${process.env.PUBLIC_URL}/home`}>
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to={`${process.env.PUBLIC_URL}/about`}>
+                About
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Switch>
+        <Route path={`${process.env.PUBLIC_URL}/home`} component={Home} />
+        <Route path={`${process.env.PUBLIC_URL}/about`} component={About} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default NavBarComp;
