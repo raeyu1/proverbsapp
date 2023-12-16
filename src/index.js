f8f56d1eff2f715cefa8d4a708ef2ec896322770
raeyu1 <89134763+raeyu1@users.noreply.github.com>
2023-12-16 12:20:34 +0800

    Update index.js
    updating index.js for link

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <Router basename="/proverbsapp">
     <App />
-  </React.StrictMode>
+  </Router>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
